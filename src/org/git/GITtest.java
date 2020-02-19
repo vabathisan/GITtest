a2c3509c4b9dd5778282fddb47e6deb1166ef231
vabathisan <vabathisan@gmail.com>
2020-02-19 18:22:19 +0530

    upload

---
+++ src/org/git/GITtest.java
@@ -0,0 +1,13 @@
+package org.git;
+
+public class GITtest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Employee name vabathisan");
+		System.out.println("Employee id 12094");
+		
+
+	}
+
+}
